115d270f56d6e93156c5f50cc0587a0ff8e2a04d
Michail Christoforatos <mchristoforatos@icloud.com>
2025-07-08 19:06:01 +0300

    fix models relationships

--- app/models.py
+++ app/models.py
@@ -6,8 +6,9 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), unique=True, index=True, nullable=False)
     password_hash = db.Column(db.String(120), nullable=False)
     created_at = db.Column(db.DateTime(timezone=True), server_default=db.func.now())
-    is_admin = db.Column(db.Boolean, index=True, nullable=False)
-    books = db.relationship("Book", backref='user')
+    is_admin = db.Column(db.Boolean, index=True, nullable=False, default=False)
+    
+    books = db.relationship("UserBook", back_populates='user')
 
     def __repr__(self):
         return f'Email: {self.email}'
@@ -17,13 +18,13 @@ class Book(db.Model):
     title = db.Column(db.String(75), nullable=False)
     published_year = db.Column(db.Integer)
     description = db.Column(db.Text())
-    isbn = db.Column(db.String(20))
+    isbn = db.Column(db.String(20), unique=True)
     cover_url = db.Column(db.String(250))
-    added_by_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     author_id = db.Column(db.Integer, db.ForeignKey('author.id'))
     created_at = db.Column(db.DateTime(timezone=True), server_default=db.func.now())
 
-    reviews = db.relationship("Review", backref="book")
+    author = db.relationship('Author', back_populates='books')
+    users = db.relationship('UserBook', back_populates='book')
 
     def __repr__(self):
         return f'Book {self.title} by {self.author.full_name}'
@@ -31,25 +32,27 @@ class Book(db.Model):
 class Author(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     full_name = db.Column(db.String(75), unique=True)
-
-    books = db.relationship("Book", backref="author")
+    books = db.relationship("Book", back_populates="author")
 
     def __repr__(self):
         return f'Author: {self.full_name}'
+    
+class UserBook(db.Model):
+    __tablename__ = 'user_book'
 
-class Review(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    book_id = db.Column(db.Integer, db.ForeignKey('book.id'))
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    book_id = db.Column(db.Integer, db.ForeignKey('book.id'), nullable=False)
+    added_at = db.Column(db.DateTime(timezone=True), server_default=db.func.now())
+
+    # review data
     rating = db.Column(db.Integer)
     review_text = db.Column(db.Text())
-    created_at = db.Column(db.DateTime(timezone=True), server_default=db.func.now())
 
-    user = db.relationship("User", backref="reviews")
+    user = db.relationship('User', back_populates='books')
+    book = db.relationship('Book', back_populates='users')
 
     __table_args__ = (
-        db.CheckConstraint('rating >= 1 AND rating <= 5', name='valid_rating'),
+        db.CheckConstraint('rating >= 1 AND rating <= 5', name='valid_rating'), # make sure ratings is within range
+        db.UniqueConstraint('user_id', 'book_id', name='uix_user_book') # prevent duplicates
     )
-
-    def __repr__(self):
-        return f'Review {self.rating}/5 by {self.user.email}'
